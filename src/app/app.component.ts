09a6c33d8f8deee2f0b0a6eda10b01ae7a55b4d2
Oscar <oscaredu01@hotmail.com>
2024-09-27 18:41:16 -0500

    feat(core): add paths in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,57 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {NavigationEnd, Router, RouterLink, RouterOutlet} from '@angular/router';
+import {HomeComponent} from "./tukun-tech/components/home/home.component";
+import {RegistrationComponent} from "./tukun-tech/components/registration/registration.component";
+import {MatButton, MatIconButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
+import {MatToolbar} from "@angular/material/toolbar";
+import {LoginComponent} from "./tukun-tech/components/login/login.component";
+import {filter} from "rxjs";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, RegistrationComponent, MatButton, MatIcon, MatIconButton, MatToolbar, RouterLink, LoginComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'tk-angular';
+
+
+  title = 'TukunTech';
+
+  showToolbar: boolean = true;
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.showToolbar = event.url !== '/login';
+      });
+  }
+
+  options = [
+    { path: '/home', title: 'Home' },
+    { path: '/registration', title: 'Registration' },
+    { path: '/patient-registration', title: 'Patient Registration' },
+    { path: '/patients', title: 'Patients'},
+    { path: '/doctors', title: 'Doctors'},
+    { path: '/postoperative', title: 'Postoperative Patients'},
+    { path: '/icu', title: 'Icu' },
+    { path: '/login', title: 'Login' },
+    { path: '/create-account', title: 'Create Account' },
+    { path: '/medical-registration', title: 'Medical Registration' },
+    { path: '/appointments', title: 'Appointments'},
+    { path: '/reshedule-appointments', title: 'Reshedule Appointment'},
+    { path: '/schedule-appointments', title: 'Schedule Appointment'},
+    { path: '/emergency-numbers', title: 'Emergency Numbers'},
+    { path: '/home-elder', title: 'Home Elder'},
+    { path: '/elder-profile', title: 'Elder Profile'},
+    { path: '/elder-reminders', title: 'Elder Reminders'},
+    { path: '/technical-support', title: 'Technical Support'},
+    { path: '/login-elder', title: 'Login Elder' },
+    { path: '/create-elder', title: 'Create Account Elder' },
+    { path: '/elder-vital-signs', title: 'Elder Vital Signs'},
+
+  ]
 }
