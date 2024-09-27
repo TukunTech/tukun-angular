c3389b494e3bc10e57bc54c15649fa9c57f9a7e2
Oscar <oscaredu01@hotmail.com>
2024-09-27 18:46:47 -0500

    feat(services): implement API calls in Doctor service

--- src/app/tukun-tech/services/doctors/doctor-api.service.ts
+++ src/app/tukun-tech/services/doctors/doctor-api.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Doctor} from "../../model/doctors/doctor.entity";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DoctorApiService {
+  baseUrl = `https://tukun-tech1.free.beeceptor.com/api/doctors`;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getDoctorInformation(){
+    return this.http.get(`https://tukun-tech1.free.beeceptor.com/api/doctors`)
+  }
+  postDoctorInformation(doctor: Doctor){
+    return this.http.post(`https://tukun-tech1.free.beeceptor.com/api/doctors`, doctor);
+  }
+
 }
