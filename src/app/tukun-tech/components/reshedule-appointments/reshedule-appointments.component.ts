2d3f185cdd03b990ce97994a4248ef3a0fc82c96
Rafooo6 <rafaelbravo032@gmail.com>
2024-09-27 19:51:03 -0500

    feat(): create reschedule-appointments component

---
+++ src/app/tukun-tech/components/reshedule-appointments/reshedule-appointments.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reshedule-appointments',
+  standalone: true,
+  imports: [],
+  templateUrl: './reshedule-appointments.component.html',
+  styleUrl: './reshedule-appointments.component.css'
+})
+export class ResheduleAppointmentsComponent {
+
+}
