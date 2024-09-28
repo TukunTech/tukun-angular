13f79ba74b92514bb2dd70eb67964ca045fa17f9
Fabio <U202218791@gmail.com>
2024-09-27 21:18:47 -0500

    feat(doctors): add home component

---
+++ src/app/tukun-tech/components/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import {Router, RouterLink} from '@angular/router';
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatButton, MatIconButton} from "@angular/material/button";
+import {MatCard} from "@angular/material/card";
+import {NgOptimizedImage} from "@angular/common";
+import {MatIcon} from "@angular/material/icon";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  standalone: true,
+  imports: [
+    MatToolbar,
+    MatButton,
+    RouterLink,
+    MatCard,
+    NgOptimizedImage,
+    MatIconButton,
+    MatIcon
+  ],
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  constructor(private router: Router) {}
+
+  navigate(route: string): void {
+    this.router.navigate([`/${route}`]);
+  }
+}
