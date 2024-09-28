47f81e7f7fde187402a4dc161d90fb7b03e660f8
Rafooo6 <rafaelbravo032@gmail.com>
2024-09-27 19:55:09 -0500

    feat(): add code in reshedule-appointments component

--- src/app/tukun-tech/components/reshedule-appointments/reshedule-appointments.component.ts
+++ src/app/tukun-tech/components/reshedule-appointments/reshedule-appointments.component.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import {MatButton} from "@angular/material/button";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatIcon} from "@angular/material/icon";
+import {MatInput} from "@angular/material/input";
+import {MatOption} from "@angular/material/core";
+import {MatRadioButton, MatRadioGroup} from "@angular/material/radio";
+import {MatSelect} from "@angular/material/select";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-reshedule-appointments',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButton,
+    MatFormField,
+    MatIcon,
+    MatInput,
+    MatLabel,
+    MatOption,
+    MatRadioButton,
+    MatRadioGroup,
+    MatSelect,
+    RouterLink
+  ],
   templateUrl: './reshedule-appointments.component.html',
   styleUrl: './reshedule-appointments.component.css'
 })
