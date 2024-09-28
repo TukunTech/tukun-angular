60820849e093ee9d6efb2870c4b61025d9f19676
adri <adriana03pch@gmail.com>
2024-09-27 19:20:35 -0500

    feat(create-account): implement create account component

---
+++ src/app/tukun-tech/components/create-account/create-account.component.ts
@@ -0,0 +1,42 @@
+import {Component, OnInit} from '@angular/core';
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {MatButton} from "@angular/material/button";
+import {RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'app-create-account',
+  standalone: true,
+  imports: [
+    MatFormField,
+    MatInput,
+    ReactiveFormsModule,
+    MatButton,
+    RouterLink,
+    MatLabel,
+  ],
+  templateUrl: './create-account.component.html',
+  styleUrl: './create-account.component.css'
+})
+export class CreateAccountComponent implements OnInit {
+  createForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    // Inicializa el formulario sin validaciones
+    this.createForm = this.fb.group({
+      name: [''],
+      ruc: [''],
+      email: [''],
+      number: [''],
+      city: [''],
+      password: ['']
+    });
+  }
+
+  ngOnInit() {}
+
+  onSubmit() {
+    console.log(this.createForm.value); // Maneja el envío del formulario
+  }
+}
