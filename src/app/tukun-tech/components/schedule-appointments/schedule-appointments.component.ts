f8b24d562a6eb2d9913b8e91bf4cb5c9867ca0d0
Rafooo6 <rafaelbravo032@gmail.com>
2024-09-27 19:56:03 -0500

    feat(): add code in shedule-appointments component

--- src/app/tukun-tech/components/schedule-appointments/schedule-appointments.component.ts
+++ src/app/tukun-tech/components/schedule-appointments/schedule-appointments.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import {MatIcon, MatIconModule} from "@angular/material/icon";
+import {MatFormField, MatFormFieldModule, MatLabel} from "@angular/material/form-field";
+import {MatCheckbox} from "@angular/material/checkbox";
+import {MatRadioButton, MatRadioGroup} from "@angular/material/radio";
+import {MatOption, MatSelect} from "@angular/material/select";
+import {MatInput, MatInputModule} from "@angular/material/input";
+import {MatButton} from "@angular/material/button";
+import {RouterLink} from "@angular/router";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatNativeDateModule} from '@angular/material/core';
 
 @Component({
   selector: 'app-schedule-appointments',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIcon,
+    MatLabel,
+    MatCheckbox,
+    MatRadioButton,
+    MatRadioGroup,
+    MatFormField,
+    MatSelect,
+    MatOption,
+    MatInput,
+    MatButton,
+    RouterLink,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatIconModule,
+    MatNativeDateModule
+  ],
   templateUrl: './schedule-appointments.component.html',
   styleUrl: './schedule-appointments.component.css'
 })
 export class ScheduleAppointmentsComponent {
 
+  onClick() {
+    alert("Information has been saved!");
+  }
 }
