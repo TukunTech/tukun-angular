7c85fd1e68cf1db4e73e40a5bd01f1cc2608619e
Rafooo6 <rafaelbravo032@gmail.com>
2024-09-27 19:52:05 -0500

    feat(): create update-appointments component

---
+++ src/app/tukun-tech/components/update-appointments/update-appointments.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-appointments',
+  standalone: true,
+  imports: [],
+  templateUrl: './update-appointments.component.html',
+  styleUrl: './update-appointments.component.css'
+})
+export class UpdateAppointmentsComponent {
+
+}
