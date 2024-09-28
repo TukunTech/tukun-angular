08aa36cc38d549b24e3c42b53efdf4293e4ef153
Fabio <U202218791@gmail.com>
2024-09-27 21:19:25 -0500

    feat(icu-pat): add icu-pat component

---
+++ src/app/tukun-tech/components/icu-pat/icu-pat.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
+import {NgForOf} from "@angular/common";
+import {Patient} from "../../model/patients/patient.entity";
+import {PatientApiService} from "../../services/patients/patient-api.service";
+import {RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'app-icu-pat',
+  standalone: true,
+  imports: [
+    MatCard,
+    MatCardContent,
+    MatCardHeader,
+    MatCardTitle,
+    NgForOf
+  ],
+  templateUrl: './icu-pat.component.html',
+  styleUrl: './icu-pat.component.css'
+})
+export class IcuPatComponent {
+  icuPatients:  Array<Patient> = [];
+  constructor(private PatientApi: PatientApiService) { }
+  getIcuPatientsInfo(){
+    this.PatientApi.getPatientInformation().subscribe((data: any) => {
+      console.log(data);
+      this.icuPatients = data;
+    })
+  }
+  ngOnInit() {
+    this.getIcuPatientsInfo();
+  }
+}
