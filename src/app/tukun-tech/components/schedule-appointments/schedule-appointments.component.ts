e40cfac0c077bd0858ce6980570f620b54e21122
Rafooo6 <rafaelbravo032@gmail.com>
2024-09-27 19:51:41 -0500

    feat(): create schedule-appointments component

---
+++ src/app/tukun-tech/components/schedule-appointments/schedule-appointments.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-schedule-appointments',
+  standalone: true,
+  imports: [],
+  templateUrl: './schedule-appointments.component.html',
+  styleUrl: './schedule-appointments.component.css'
+})
+export class ScheduleAppointmentsComponent {
+
+}
