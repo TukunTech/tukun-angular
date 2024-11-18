9249ec916c73ec872788ded539d72c4ca358fd45
Rafooo6 <rafaelbravo032@gmail.com>
2024-11-17 22:43:09 -0500

    feat(technical-support): implement logic for technical support component

--- src/app/tukun-tech/components/technical-support/technical-support.component.ts
+++ src/app/tukun-tech/components/technical-support/technical-support.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, Input, OnInit} from '@angular/core';
+import {CommonModule, NgForOf} from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import {RouterLink} from "@angular/router";
 import {TranslateModule} from "@ngx-translate/core";
+import {TechnicalSupport} from "../../model/technicalSupport/technical-support";
+import {FormBuilder} from "@angular/forms";
+import {TechnicalSupportService} from "../../services/technicalSupport/technical-support.service";
 
 @Component({
   selector: 'app-technical-support',
@@ -17,13 +20,27 @@ import {TranslateModule} from "@ngx-translate/core";
     MatInputModule,
     MatCardModule,
     RouterLink,
-    TranslateModule
+    TranslateModule,
+    NgForOf
   ],
   templateUrl: './technical-support.component.html',
   styleUrl: './technical-support.component.css'
 })
-export class TechnicalSupportComponent {
-  faqs = ['Question 1', 'Question 2', 'Question 3', 'Question 4'];
-  supportNumber = '1234567890';
+export class TechnicalSupportComponent implements OnInit{
+  technicalSupports: Array<TechnicalSupport> = [];
+  supportNumber = 948129421;
+
+  constructor(private technicalApi: TechnicalSupportService) {}
+
+  getTechnicalSupport() {
+    this.technicalApi.getTechnicalSupport().subscribe((data: any) => {
+      console.log(data);
+      this.technicalSupports = data;
+    })
+  }
+
+  ngOnInit() {
+    this.getTechnicalSupport()
+  }
 
 }
