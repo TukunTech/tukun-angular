81ecff1a145afe9e6bc81e1abba616fa32cb70b2
Rafooo6 <rafaelbravo032@gmail.com>
2024-11-18 17:41:22 -0500

    feat(pending-medicine): implement service for PendingMedicine management

---
+++ src/app/tukun-tech/services/pending-medicine/pending-medicine.service.ts
@@ -0,0 +1,24 @@
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Doctor} from "../../model/doctors/doctor.entity";
+import {PendingMedicine} from "../../model/pending-medicine/pending-medicine";
+
+export class PendingMedicineService {
+  baseUrl = 'http://localhost:8080/pendingMedicine/v1';
+
+  constructor(private http: HttpClient) { }
+
+  getMedicineInformation(): Observable<any>{
+    return this.http.get(this.baseUrl+'/medicines')
+  }
+
+  postMedicineInformation(medicine: PendingMedicine): Observable<any>{
+    return this.http.post(this.baseUrl, medicine);
+  }
+  putMedicine(medicine: PendingMedicine): Observable<any>{
+    return this.http.put(this.baseUrl +'/' + medicine.id, medicine);
+  }
+  deleteMedicine(id: number): Observable<any>{
+    return this.http.delete(this.baseUrl+'/'+id);
+  }
+}
