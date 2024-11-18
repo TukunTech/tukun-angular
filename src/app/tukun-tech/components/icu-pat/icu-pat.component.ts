eb22029fe676756c8f193023e558b150c01de1dc
adri <adriana03pch@gmail.com>
2024-11-17 22:12:34 -0500

    feat: update icu patient component

--- src/app/tukun-tech/components/icu-pat/icu-pat.component.ts
+++ src/app/tukun-tech/components/icu-pat/icu-pat.component.ts
@@ -1,33 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
 import {NgForOf} from "@angular/common";
 import {Patient} from "../../model/patients/patient.entity";
 import {PatientApiService} from "../../services/patients/patient-api.service";
 import {RouterLink} from "@angular/router";
+import {BedService} from "../../services/patients/bed.service";
+import {Bed} from "../../model/patients/bed";
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-icu-pat',
   standalone: true,
-    imports: [
-        MatCard,
-        MatCardContent,
-        MatCardHeader,
-        MatCardTitle,
-        NgForOf
-    ],
+  imports: [
+    MatCard,
+    MatCardContent,
+    MatCardHeader,
+    MatCardTitle,
+    NgForOf,
+    TranslateModule
+  ],
   templateUrl: './icu-pat.component.html',
   styleUrl: './icu-pat.component.css'
 })
-export class IcuPatComponent {
-  icuPatients:  Array<Patient> = [];
-  constructor(private PatientApi: PatientApiService) { }
-  getIcuPatientsInfo(){
-    this.PatientApi.getPatient().subscribe((data: any) => {
-      console.log(data);
-      this.icuPatients = data;
-    })
+
+export class IcuPatComponent implements OnInit{
+
+  // grilla
+  beds: Bed[] =[];
+
+  constructor(private bedService: BedService) {
   }
+
   ngOnInit() {
-    this.getIcuPatientsInfo();
+    this.consulta();
   }
+
+  consulta() {
+    this.bedService.getBed().subscribe(
+      x => this.beds = x
+    )
+  }
+
+
 }
