30d416252a50502a473699bf4f629aca7dd68509
Rafooo6 <rafaelbravo032@gmail.com>
2024-11-18 23:33:43 -0500

    feat(elder-reminders): add functionality to manage and display elder reminders

--- src/app/tukun-tech/components/elder-reminders/elder-reminders.component.ts
+++ src/app/tukun-tech/components/elder-reminders/elder-reminders.component.ts
@@ -6,6 +6,7 @@ import { MatCardModule } from '@angular/material/card';
 import {RouterLink} from "@angular/router";
 import {PendingMedicine} from "../../model/pending-medicine/pending-medicine";
 import {PendingMedicineService} from "../../services/pending-medicine/pending-medicine.service";
+import Swal from "sweetalert2";
 
 
 @Component({
@@ -37,6 +38,34 @@ export class ElderRemindersComponent implements OnInit {
     )
   }
 
+  deletePendingMedicines(obj: PendingMedicine){
+
+    Swal.fire({
+      title: '¿Desea eliminar?',
+      text: "Los cambios no se van a revertir",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, elimina.',
+      cancelButtonText: 'No, cancelar'
+    }).then((result) => {
+      if(result.isConfirmed){
+        this.pendingMedicineService.deleteMedicine(obj.id || 0).subscribe(
+          x => {
+            Swal.fire('Medicina Pendiente eliminada', x.mensaje, 'success');
+            this.pendingMedicineService.getMedicine().subscribe(
+              x => this.pendingMedicines = x
+            );
+          }
+        );
+      }
+    })
+
+
+
+
+  }
 
 
 }
