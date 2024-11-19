ba53033b8356d11203602c3c055118a9be3e71bf
Rafooo6 <rafaelbravo032@gmail.com>
2024-11-18 23:21:04 -0500

    feat(pending-medicine): update medicine model with additional attributes

--- src/app/tukun-tech/model/pending-medicine/pending-medicine.ts
+++ src/app/tukun-tech/model/pending-medicine/pending-medicine.ts
@@ -1,18 +1,24 @@
 import {Status} from "./status";
 
+class Elder {
+}
+
 export class PendingMedicine {
   id?: number;
   medicineName?: string;
   status?: Status;
   dosage?: string;
   timeToTake?: Date;
+  elder?: Elder;
 
-  constructor(id: number, medicineName: string, status: Status, dosage: string) {
+  constructor(id: number, medicineName: string, status: Status, dosage: string, timeToTake: Date, elder: Elder) {
 
     this.id = id;
     this.medicineName = medicineName;
     this.status = status;
     this.dosage = dosage;
+    this.timeToTake = timeToTake;
+    this.elder = elder;
 
   }
 
