22defb78ca6611e1a4617555a5fa702093542a31
Rafooo6 <rafaelbravo032@gmail.com>
2024-11-18 23:20:33 -0500

    feat(elder-reminders): implement functionality to manage elder reminders

--- src/app/tukun-tech/services/pending-medicine/pending-medicine.service.ts
+++ src/app/tukun-tech/services/pending-medicine/pending-medicine.service.ts
@@ -2,23 +2,19 @@ import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Doctor} from "../../model/doctors/doctor.entity";
 import {PendingMedicine} from "../../model/pending-medicine/pending-medicine";
+import {Injectable} from "@angular/core";
 
+@Injectable({
+  providedIn: 'root'
+})
 export class PendingMedicineService {
   baseUrl = 'http://localhost:8080/api/v1/pendingMedicine';
 
   constructor(private http: HttpClient) { }
 
-  getMedicineInformation(): Observable<any>{
-    return this.http.get(this.baseUrl+'/medicines')
+  getMedicine(): Observable<any>{
+    return this.http.get(this.baseUrl+'/pendingMedicines')
   }
 
-  postMedicineInformation(medicine: PendingMedicine): Observable<any>{
-    return this.http.post(this.baseUrl, medicine);
-  }
-  putMedicine(medicine: PendingMedicine): Observable<any>{
-    return this.http.put(this.baseUrl +'/' + medicine.id, medicine);
-  }
-  deleteMedicine(id: number): Observable<any>{
-    return this.http.delete(this.baseUrl+'/'+id);
-  }
+
 }
